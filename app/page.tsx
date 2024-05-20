e3911222d551fe97b6150f78dd90b4eac4c8c16f
Ilan Krayn <37673353+ilankr@users.noreply.github.com>
2024-05-20 16:17:13 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
       <p>this is text text</p>
-
-      
-          </main>
+      <h2>These are notes from Ilan Krayn for Skills Assessment</h2>
+      <h3>Item 1</h3>
+      <p>5 favorite support tasks</p>
+      <ul>
+        <li>Manage a support team</li>
+        <li>Find and recruit teammates for the support team </li>
+        <li>Help train and onboard new support teammates</li>
+        <li>Work with the product team to develop a newfeature based on feedback from customers</li>
+        <li>Analyze hundreds of support tickets to spot trends the product team can use</li>
+      </ul>
+      <p>5 least favorite support tasks</p>
+      <p>There is really nothing on this list that I don't enjoy doing.</p>
+      <hr>
+    </main>
   );
 }
