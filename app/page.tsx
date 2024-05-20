d7db4a9923d892bf145d7a444768f59160a41560
Ilan Krayn <37673353+ilankr@users.noreply.github.com>
2024-05-20 15:21:54 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <p>this is text text</p>
 
-              <h2>These are notes from Ilan Krayn for Skills Assessment</h2>
-
-<h3>Item 1</h3>
-
-<p>5 favorite support tasks</p>
-
-<ul>
-        <li>Manage a support team</li>
-<li>Find and recruit teammates for the support team </li>
-<li>Help train and onboard new support teammates</li>
-<li>Work with the product team to develop a newfeature based on feedback from customers</li>
-<li>Analyze hundreds of support tickets to spot trends the product team can use</li>
-</ul>
-
-<p>5 least favorite support tasks</p>
-There is really nothing on this list that I don't enjoy doing.
-<hr>
-<h3>Item 2</h3>
-
-<p>What I'll need to learn to work
-at Vercel is front end, JavaScript development. This
-is not an area that I focused on before, and clearly, I will need to improve in
-it so I can do a great job here.</p>
-
-<hr>
-    
-    </main>
+      
+          </main>
   );
 }
