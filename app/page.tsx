942510bd4e36004f2a6e9bf3e1ebb30d25b8d143
Ilan Krayn <37673353+ilankr@users.noreply.github.com>
2024-05-20 17:15:06 -0700

    Item 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-left justify-between p-24">
-    
-      <p>this is text text</p>
       <h2>These are notes from Ilan Krayn for Skills Assessment</h2>
       <h3>Item 1</h3>
       <p>5 favorite support tasks</p>
@@ -24,6 +22,23 @@ export default function Home() {
         is not an area that I focused on before, and clearly, I will need to improve in
         it so I can do a great job here.</p>
       <p>------------</p>
+      <h3>Item 3</h3>
+      <p>At Harness I was working with a banking customer facing
+        failed deployment and was not able to understand why (this was a very common
+        support issue). Since this was a P1 issue, we had a call going to work on this.
+        Looking at our agent logs I found the root issue and showed the customer how to
+        fix it. The customer immediately tried my suggestion, and the deployment was
+        completed successfully. Once it was fixed, I showed the customer how I was able
+        to find the problem so in the future they can troubleshoot themselves and solve
+        issues faster.</p>
+        <p>Another example, At Aisera I worked with a large national retail store chain.
+        They felt that they were not getting value from the product. Working together
+        with the dedicated CSE of the account we helped figure out what features they
+        were not using to get value and walked the customer through configuring them. This
+        allowed us to turn a churn risk customer into a happy, renewing one.</p>
+     <p>------------</p>
+
+
       
     </main>
   );
